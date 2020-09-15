6ba1f37e2eb01e76b4ff3441dcc545f8ca24f7b1
JH-Seo2020 <71337347+JH-Seo2020@users.noreply.github.com>
2020-09-16 02:19:19 +0900

    BasicSetting_0916_서지혜
    1. JDBCTemplate 미완성
    2. 서버, 메타데이터, 클래스파일 ignore 처리함
    3, OJDBC6 파일 lib에 미리 추가해둠
    4. spl문 담을 파일들 전부 xml로 설정함(계정명 지어야합니다)
    5. 깃 초대 메일 봐주세요

---
+++ TicketPopGet/src/com/kh/common/JDBCTemplate.java
@@ -0,0 +1,23 @@
+package com.kh.common;
+
+public class JDBCTemplate {
+
+	//static 메소드 6개 작성 예정
+	//driver 파일을 xml로 만들었기 때문에 확장자, loadFromXML 메소드사용하기
+	
+	//...한번 적다 날아가서 일단 비워둡니다.
+	//제가 쓰던지, 같이쓰던지, 사다리를 타던지 했음 좋겠습니다
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
