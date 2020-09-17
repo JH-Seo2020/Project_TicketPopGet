7de9b6f904e345bf685def4b90203ae7f572389c
hyunsun-leah <khs01231004@naver.com>
2020-09-17 12:01:32 +0900

    JDBCTemplate_0917_김현선
    JDBCTemplate.java 작성완료

--- TicketPopGet/src/com/kh/common/JDBCTemplate.java
+++ TicketPopGet/src/com/kh/common/JDBCTemplate.java
@@ -1,28 +1,109 @@
 package com.kh.common;
 
-public class JDBCTemplate {
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
 
-	//static 메소드 6개 작성 예정
-	//driver 파일을 xml로 만들었기 때문에 확장자, loadFromXML 메소드사용하기
+/** static 메소드 6개 작성 예정
+ * @author 김현선
+ *
+ */
+public class JDBCTemplate {
 	
-	
+	// 1. Connection 객체 생성 후 해당 Connection 반환해주는 getConnection 메소드
 	public static Connection getConnection() {
 		
 		Properties prop = new Properties();
 		
+		//driver 파일을 xml로 만들었기 때문에 확장자, loadFromXML 메소드사용하기
+		String fileName = JDBCTemplate.class.getResource("/sql/driver/driver.xml").getPath();
 		
+		try {
+			prop.load(new FileInputStream(fileName));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		Connection conn = null;
+		
+		try {
+			Class.forName(prop.getProperty("driver"));
+			
+			conn = DriverManager.getConnection(prop.getProperty("url"),
+											   prop.getProperty("username"),
+											   prop.getProperty("password"));
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return conn;
 	}
 	
+	// 2. Connection 객체 전달 받아서 commit해주는 commit 메소드
+	public static void commit(Connection conn) {
+		
+		try {
+			if(conn != null && conn.isClosed()) {
+				conn.commit();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 	
+	// 3. Connection 객체 전달 받아서 rollback해주는 rollback 메소드
+	public static void rollback(Connection conn) {
+		
+		try {
+			if(conn != null && !conn.isClosed()) {
+				conn.rollback();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 	
+	// 4. Connection 객체 전달 받아서 close 해주는  close 메소드
+	public static void close(Connection conn) {
+		
+		try {
+			if(conn != null && !conn.isClosed()) {
+				conn.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}	
+	}
 	
+	// 5. Statment관련 객체 전달받아서 close 해주는 close 메소드
+	public static void close(Statement stmt) {
+		
+		try {
+			if(stmt != null && !stmt.isClosed()) {
+				stmt.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 	
-	
-	
-	
-	
-	
-	
-	
+	// 6. ResultSet 객체 전달받아서 close해주는 close 메소드
+	public static void close(ResultSet rset) {
+		
+		try {
+			if(rset != null && !rset.isClosed()) {
+				rset.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 	
 }
