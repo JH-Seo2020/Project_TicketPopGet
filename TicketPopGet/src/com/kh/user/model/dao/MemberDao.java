760af595d5e3afca78aa18034ac7cbca72c60195
hyunsun-leah <khs01231004@naver.com>
2020-09-24 15:54:19 +0900

    0924_MemberDao수정_김현선
    MemberDao > loginMember 추가

--- TicketPopGet/src/com/kh/user/model/dao/MemberDao.java
+++ TicketPopGet/src/com/kh/user/model/dao/MemberDao.java
@@ -2,7 +2,13 @@ package com.kh.user.model.dao;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Properties;
+
+import com.kh.user.model.vo.Member;
 
 import static com.kh.common.JDBCTemplate.*;
 
@@ -10,6 +16,9 @@ public class MemberDao {
 	
 	private Properties prop = new Properties();
 	
+	/**
+	 * @author 김현선
+	 */
 	public MemberDao() {
 		
 		String fileName = MemberDao.class.getResource("/sql/user/user-mapper.xml").getPath();
@@ -19,6 +28,57 @@ public class MemberDao {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+
+	/**
+	 * @author 김현선
+	 * @param conn
+	 * @param userId
+	 * @param userPwd
+	 * @return
+	 */
+	public Member loginMember(Connection conn, String userId, String userPwd) {
+		
+		Member m = null;
+		
+		PreparedStatement pstmt = null;
+		ResultSet rset = null;
+		
+		String sql = prop.getProperty("loginMember");
+		
+		try {
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setString(1, userId);
+			pstmt.setString(2, userPwd);
+			
+			rset = pstmt.executeQuery();
+			
+			if(rset.next()) {
+				m = new Member(rset.getInt("USER_NO"),
+						       rset.getString("USER_ID"),
+						       rset.getString("USER_PWD"),
+						       rset.getString("USER_NAME"),
+						       rset.getString("EMAIL"),
+						       rset.getString("PHONE"),
+						       rset.getDate("BIRTHDATE"),
+						       rset.getString("GENDER"),
+						       rset.getDate("DELETE_DATE"),
+						       rset.getString("DELETE_STATUS"),
+						       rset.getString("BLACKLIST_STATUS"),
+						       rset.getInt("REPORT_COUNT"),
+						       rset.getString("DELETE_REASON"));				       )
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close(rset);
+			close(pstmt);
+		}
+		
+		return m;
+
 	}
 
 }
