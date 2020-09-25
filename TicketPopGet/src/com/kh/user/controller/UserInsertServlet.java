df8df73634ac925baa1f0de4e3c4ccd86608a578
hyunsun-leah <khs01231004@naver.com>
2020-09-26 03:44:26 +0900

    0926_회원가입insert서블릿_김현선
    회원가입 시 insert 서블릿 생성

---
+++ TicketPopGet/src/com/kh/user/controller/UserInsertServlet.java
@@ -0,0 +1,50 @@
+package com.kh.user.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class UserInsertServlet
+ */
+@WebServlet("/insert.me")
+public class UserInsertServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UserInsertServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		request.setCharacterEncoding("UTF-8");
+		
+		String userId = request.getParameter("userId");		// 아이디
+		String userPwd = request.getParameter("userPwd");	// 비밀번호
+		String userName = request.getParameter("userName");	// 이름
+		String email = request.getParameter("email");		// 이메일
+		String phone = request.getParameter("phone");		// 전화번호
+		//생년월일
+		//성별
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
