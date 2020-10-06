5f4f7d3af95b4e242b33bdefd81f3c597690b3e9
hyunsun-leah <khs01231004@naver.com>
2020-10-07 01:23:40 +0900

    1007_중간저장_김현선

--- TicketPopGet/src/com/kh/serviceCenter/controller/questionDetailServlet.java
+++ TicketPopGet/src/com/kh/serviceCenter/controller/questionDetailServlet.java
@@ -31,7 +31,6 @@ public class questionDetailServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println(request.getParameter("qno"));
 		
 		int qno = Integer.parseInt(request.getParameter("qno"));
 		
